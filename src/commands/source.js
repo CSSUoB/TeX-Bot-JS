d10f07a6786661d7dc69f1d4b66558390bf6078b
Adam Hodgkinson <adam@adamhodgkinson.dev>
2022-10-08 21:36:05 +0100

    fixed grammar

--- src/commands/source.js
+++ src/commands/source.js
@@ -5,6 +5,6 @@ module.exports = {
     .setName("source")
     .setDescription("Displays information about the source code of this bot."),
   async execute(interaction) {
-    await interaction.reply("TeX bot is an open-source project made specifically for the CSS Discord! You can see and contribute the source code at https://github.com/CSSUoB/TeX-Bot-JS");
+    await interaction.reply("TeX is an open-source project made specifically for the CSS Discord! You can see and contribute to the source code at https://github.com/CSSUoB/TeX-Bot-JS");
   },
 };
